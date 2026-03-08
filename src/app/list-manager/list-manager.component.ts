fe8a9d5891eb990cb90008e0875a3f0d64d1a3ad
YaMe09 <134693503+YaMe09@users.noreply.github.com>
2026-03-08 16:19:56 +0100

    create ny page

---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-manager.component.html',
+  styleUrl: './list-manager.component.css'
+})
+export class ListManagerComponent {
+
+}
