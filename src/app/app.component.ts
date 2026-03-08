09fc161aff2bc8c38b26b583765c48b089ffd5c8
YaMe09 <134693503+YaMe09@users.noreply.github.com>
2026-03-08 14:56:31 +0100

    create Angular to do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [InputButtonUnitComponent],
+  template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+    <app-input-button-unit></app-input-button-unit>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ngGrils-app';
